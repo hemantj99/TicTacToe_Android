2885fd8e83e8c8bcfbfa4417c2438f0919e8941c
hemantj99 <hemantjain1999@gmail.com>
2020-09-19 23:46:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.tictactoe;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button Reset;
+    boolean gameActive = true;
+    int activePlayer = 0;
+    int count=0;
+    int[] gameState = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
+    //    State meanings:
+    //    0 - X
+    //    1 - O
+    //    -1 - Null
+    int[][] winPositions = {{0,1,2}, {3,4,5}, {6,7,8},
+            {0,3,6}, {1,4,7}, {2,5,8},
+            {0,4,8}, {2,4,6}};
+    public void playatap(View view){
+        ImageView img = (ImageView) view;
+        int tappedImage = Integer.parseInt(img.getTag().toString());
+        count+=1;
+        if(!gameActive){
+            gameReset(view);
+        }
+        if(gameState[tappedImage] == -1) {
+            gameState[tappedImage] = activePlayer;
+            if (activePlayer == 0)
+            {
+                img.setImageResource(R.drawable.x);
+                activePlayer = 1;
+                TextView status = findViewById(R.id.status);
+                status.setText("O's Turn - Tap to play");
+            }
+            else
+            {
+                img.setImageResource(R.drawable.o);
+                activePlayer = 0;
+                TextView status = findViewById(R.id.status);
+                status.setText("X's Turn - Tap to play");
+            }
+
+        }
+        // Check Which player has won
+        for(int[] winPosition: winPositions)
+        {
+            if (gameState[winPosition[0]] == gameState[winPosition[1]] &&
+                    gameState[winPosition[1]] == gameState[winPosition[2]] &&
+                    gameState[winPosition[0]] != -1)
+            {
+
+                String win;
+                gameActive = false;
+                if (gameState[winPosition[0]] == 0)
+                {
+                    win = "X has won";
+                    count=0;
+
+                }
+                else
+                {
+                    win = "O has won";
+                    count=0;
+
+                }
+                TextView status = findViewById(R.id.status);
+                status.setText(win);
+            }
+        }
+        if(count==9)
+        {
+            TextView status = findViewById(R.id.status);
+            status.setText("Draw");
+            gameActive = false;
+
+        }
+    }
+    public void gameReset(View view)
+    {
+        gameActive = true;
+        activePlayer = 0;
+        for(int i=0; i<gameState.length; i++)
+        {
+            gameState[i] = -1;
+        }
+        count=0;
+        ((ImageView)findViewById(R.id.imageView0)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView1)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView2)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView3)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView4)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView5)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView6)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView7)).setImageResource(0);
+        ((ImageView)findViewById(R.id.imageView8)).setImageResource(0);
+
+        TextView status = findViewById(R.id.status);
+        status.setText("X's Turn - Tap to play");
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Reset = (Button) findViewById(R.id.Reset);
+        Reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gameReset(view);
+            }
+        });
+    }
+}
+
