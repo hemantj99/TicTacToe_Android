25d01a5f2aa14b299ad3798fd91831846656f890
hemantj99 <hemantjain1999@gmail.com>
2021-04-03 14:14:42 +0530

    New Screen Added

--- app/src/main/java/com/hemant/tictactoe/StartActivity.java
+++ app/src/main/java/com/hemant/tictactoe/StartActivity.java
@@ -2,13 +2,31 @@ package com.hemant.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.hemant.tictactoe.R;
 
 public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        getSupportActionBar().hide();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartActivity.this ,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 25000);
     }
 }
